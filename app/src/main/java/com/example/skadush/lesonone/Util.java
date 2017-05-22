4ac339a41d2485d9abfb072defba49fe90d05f51
Nelzkie <nelson.altares@gmail.com>
2017-05-22 20:28:50 +1200

    Lesson2

--- app/src/main/java/com/example/skadush/lesonone/Util.java
+++ app/src/main/java/com/example/skadush/lesonone/Util.java
@@ -1,6 +1,8 @@
 package com.example.skadush.lesonone;
 
 import android.content.Context;
+import android.opengl.GLES20;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -40,5 +42,38 @@ public class Util {
         }
 
         return body.toString();
+    }
+
+    private int compileShader(final int shaderType, final String shaderSource)
+    {
+        int shaderHandle = GLES20.glCreateShader(shaderType);
+
+        if (shaderHandle != 0)
+        {
+            // Pass in the shader source.
+            GLES20.glShaderSource(shaderHandle, shaderSource);
+
+            // Compile the shader.
+            GLES20.glCompileShader(shaderHandle);
+
+            // Get the compilation status.
+            final int[] compileStatus = new int[1];
+            GLES20.glGetShaderiv(shaderHandle, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+            // If the compilation failed, delete the shader.
+            if (compileStatus[0] == 0)
+            {
+                Log.e("Shader", "Error compiling shader: " + GLES20.glGetShaderInfoLog(shaderHandle));
+                GLES20.glDeleteShader(shaderHandle);
+                shaderHandle = 0;
+            }
+        }
+
+        if (shaderHandle == 0)
+        {
+            throw new RuntimeException("Error creating shader.");
+        }
+
+        return shaderHandle;
     }
 }
