4a82e954db0ee5af53bd63643dc8ea7b2f092055
Nelzkie <nelson.altares@gmail.com>
2017-05-22 20:41:24 +1200

    Modified Vertex and fragment

--- app/src/main/res/raw/vertex.glsl
+++ app/src/main/res/raw/vertex.glsl
@@ -2,14 +2,14 @@
 
 uniform mat4 u_MVPMatrix;
 
-attribute vec4 a_Position; //ffs
-attribute vec4 a_Color;
+attribute vec4 a_Position;
 
-varying vec4 v_Color;
+
 
 void main() {
-   v_Color = a_Color;
+
 
 
     gl_Position = u_MVPMatrix * a_Position;
+    gl_PointSize = 5.0;
 }
